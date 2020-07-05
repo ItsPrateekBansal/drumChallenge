1d4d7900319b87858b64b414603e57658b990e15
prateek <prateekbansal64@gmail.com>
2020-07-05 23:12:05 +0530

    Drum is Ready

---
+++ index.js
@@ -0,0 +1,24 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+var maps = {
+    "w":"tom-1.mp3",
+    "a":"tom-2.mp3",
+    "s":"tom-3.mp3",
+    "d":"tom-4.mp3",
+    "j":"snare.mp3",
+    "k":"crash.mp3",
+    "l":"kick-bass.mp3"
+}
+document.addEventListener('keypress',logkey);
+function logkey(e){
+    var ke = e.key;
+    let audio_val = "sounds/"+maps[ke];
+    var audio = new Audio(audio_val);
+    audio.play();
+}
+
+
+
+
+
+// var audio = new Audio('sounds/tom-1.mp3')
+//         audio.play();
